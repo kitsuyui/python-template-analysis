8edaf8c0d8b8e9971f5b02bbd77ff4b8975cc1f3
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-06-01 18:35:56 +0900

    fix: export AnalyzerResult public return types

--- template_analysis/__init__.py
+++ template_analysis/__init__.py
@@ -7,10 +7,20 @@ This library takes a formatted string and returns a template and data."""
 # https://packaging-guide.openastronomy.org/en/latest/advanced/versioning.html
 from ._version import __version__
 from .analyzer import Analyzer, AnalyzerResult, analyze
+from .symbol import Chunks, Symbol, SymbolString, SymbolTable
+from .template import PlainText, Template, TemplatePart, Variable
 
 __all__ = [
     "Analyzer",
     "AnalyzerResult",
+    "Chunks",
+    "PlainText",
+    "Symbol",
+    "SymbolString",
+    "SymbolTable",
+    "Template",
+    "TemplatePart",
+    "Variable",
     "__version__",
     "analyze",
 ]
